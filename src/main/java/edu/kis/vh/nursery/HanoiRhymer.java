88ad09d8b0ac645ca123c84a1a6b3d79a783b3e5
Szymon Zajac <moniukins@gmail.com>
2021-12-03 10:58:41 +0100

    1.3.2 Popraw formatowanie plików

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,19 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+	int totalRejected = 0;
 
 	public int reportRejected() {
 		return totalRejected;
 	}
 
 	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
+		if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+		else
+			super.countIn(in);
 	}
 }
+
+// Sformatowano linie 5, 12, 14, 15.
+// alt + strzałki przechodzi do ostatnio edytowanych linii chronologicznie
