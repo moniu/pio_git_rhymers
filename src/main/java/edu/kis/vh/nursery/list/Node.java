56295a11129258a12e9ac9df77a4cbe3a9ceae7e
Szymon Zajac <moniukins@gmail.com>
2022-01-18 13:15:50 +0100

    1.4 Accessory 3 - Encapsulated public attributes

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,9 +1,34 @@
 package edu.kis.vh.nursery.list;
 
 public class Node {
+	
+	private int value;
+	private Node prev;
+    private Node next;
 
-	protected int value;
-	protected Node prev, next;
+	public int getValue() {
+		return this.value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getPrev() {
+		return this.prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return this.next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 
 	public Node(int i) {
 		value = i;
